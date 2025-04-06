9640eaee7d39bbd31018bbe45190c398244a5f0a
nblitte <nathalie.blitte@gmail.com>
2025-04-06 19:36:09 +0200

    Ajout de la fonction numerotation_cases

--- my_package/plan_chateau.py
+++ my_package/plan_chateau.py
@@ -31,7 +31,8 @@ def calculer_pas(matrice):
      Calcul la dimension à donner aux cases pour que le plan tienne dans la zone de la fenêtre
      turtle que nous aurons défini
     :param matrice: Résultat de la fonction lire_matrice()
-    :return: Dimensions en largeur et hauteur appelé "pas", nombre de cases
+    :return: Dimensions en largeur et hauteur appelé "pas", nombre de cases de la fenêtre Turtle
+    en largeur, en hauteur et le nombre de cases total
     """
     # Calcul de la longueur et de la largeur de la matrice du château
     long_M = len(matrice) # 27
@@ -64,7 +65,43 @@ def calculer_pas(matrice):
     # Calcul du nombre de cases de la fenêtre Turtle
     nb_cases_total = nb_case_la_pixels * nb_case_ha_pixels # 551
 
-    return pas, nb_cases_total
+    return pas, nb_case_la_pixels, nb_case_ha_pixels, nb_cases_total
+
+def numerotation_cases(matrice, case, pas):
+    """
+    Numérotation des cases du château par un système de coordonnées
+    :param case: Résultat de la fonction lire_matrice()
+    :param pas: Résultat de la fonction calculer_pas()
+    :return: Position de la case dans la fenêtre Turtle sous forme de tuple (x,y) :
+    numéro de la ligne et de colonne (cf §7.1.3. En pratique)
+    """
+
+
+    # NE FONCTIONNE PAS #
+    coord_x = 0
+    coord_y = 0
+    num_ligne = 1
+    num_colonne = 1
+
+    # Calcul de la longueur et de la largeur de la matrice du château
+    long_M = len(matrice) # 27
+    larg_M = 0            # 19
+
+    for y in range(case[1]):
+        larg_M = len(matrice[y])
+        if coord_y < larg_M:
+            chateau = [y]
+            chateau.append(coord_y)
+        elif coord_y >= larg_M:
+            chateau = [y]
+            chateau.append(coord_y + 1)
+            num_colonne += 1
+    print("Coordonnées de y :", coord_y)
+    print("Numéro de colonne :", num_colonne)
+    print(chateau)
+
+
+
 
 def coordonnees(case, pas):
     """
@@ -75,6 +112,27 @@ def coordonnees(case, pas):
     :return: Coordonnées en pixels turtle du coin inférieur gauche d’une case (numéro de ligne
      et de colonne)
     """
+
+    coord_x = ZONE_PLAN_MINI[0] + pas
+    coord_y = ZONE_PLAN_MAXI[1] - pas
+    num_ligne = 0
+
+
+    for x in range(larg_M):
+        coord_x = case[0] + pas
+        num_ligne = x + 1
+        if coord_x >= ZONE_PLAN_MAXI[0]:
+            num_ligne = num_ligne + 1
+            coord_x = pas
+
+
+    print("Coordonnées x:", coord_x)
+    print("Numéro de ligne :", num_ligne) # Ne fonctionne pas
+
+
+
+
+
 
 def tracer_carre(dimension):
     """
@@ -103,5 +161,16 @@ def afficher_plan(matrice):
     """
 
 # MAIN DU PROGRAMME
-resultat = lire_matrice("plan_chateau.txt")
-calculer_pas(resultat)
+# Lecture du fichier plan_chateau.txt
+matrice = lire_matrice("plan_chateau.txt")
+
+# Calcul de la dimension (pas) d'une case et du nombre total de cases de la fenêtre Turtle
+pas, nb_case_largeur, nb_case_hauteur, nb_case_total = calculer_pas(matrice)
+print(nb_case_largeur)
+print(nb_case_hauteur)
+
+# Donne les coordonnées en pixels turtle du coin inférieur gauche d’une case (numéro de ligne
+#      et de colonne)
+case = (19,26) # (-240,-240)
+
+numerotation_cases(matrice, case, pas)
